fcf1dc87950319666e9e1fb6bffd3ca4f9c5e94d
Li Lin <guru.lin@gmail.com>
2023-10-22 18:13:55 +0800

    rename test method

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -3,7 +3,7 @@ import os
 import openai
 
 
-def test_completion():
+def test_openai_completion():
     prompt = "Hello world"
     model_name = os.environ.get("OPENAI_DEFAULT_MODEL", "gpt-3.5-turbo")
     completion = openai.ChatCompletion.create(model=model_name, messages=[{"role": "user", "content": prompt}])
